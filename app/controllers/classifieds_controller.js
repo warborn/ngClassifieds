8bb2107c0ab8f922d8673d9fa0b03c57132bc7d3
Ivan Munguia <xxthecreatorxx_live@hotmail.com>
2016-04-21 04:02:55 +0000

    Add create new classified functionality

--- app/controllers/classifieds_controller.js
+++ app/controllers/classifieds_controller.js
@@ -1,8 +1,14 @@
 (function() {
   'use strict';
 
-  var classifiedsCtrl = function($scope, $http, Classified, $mdSidenav) {
+  var classifiedsCtrl = function($scope, $http, Classified, $mdSidenav, $mdToast) {
     $scope.showAdmin = true;
+
+    var contact = {
+      name: 'Ryan Chenkie',
+      phone: '(444) 444-4444',
+      email: 'ryian@gmail.com'
+    };
 
     Classified.getAll()
       .then(function(classifieds) {
@@ -16,9 +22,25 @@
     $scope.closeSidenav = function() {
       $mdSidenav('left').close();
     };
+
+    $scope.saveClassified = function(classified) {
+      if(classified) {
+        classified.contact = contact;
+        classified.posted = new Date().toJSON().slice(0,10);
+        $scope.classifieds.push(classified);
+        $scope.classified = {};
+        $scope.closeSidenav();
+        $mdToast.show(
+          $mdToast.simple()
+            .content("Classified Saved!")
+            .position('top, right')
+            .hideDelay(3000)
+        );
+      }
+    };
   };
 
-  classifiedsCtrl.$inject = ['$scope', '$http', 'Classified', '$mdSidenav'];
+  classifiedsCtrl.$inject = ['$scope', '$http', 'Classified', '$mdSidenav', '$mdToast'];
 
   angular.module('ngClassifieds')
     .controller('ClassifiedsCtrl', classifiedsCtrl);
