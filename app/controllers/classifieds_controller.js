853313e22ed6a73fd27a09ab0f7f6e4ea2a2c5be
Ivan Munguia <xxthecreatorxx_live@hotmail.com>
2016-04-21 02:25:49 +0000

    Add sidenav

--- app/controllers/classifieds_controller.js
+++ app/controllers/classifieds_controller.js
@@ -1,16 +1,24 @@
 (function() {
   'use strict';
 
-  var classifiedsCtrl = function($scope, $http, Classified) {
+  var classifiedsCtrl = function($scope, $http, Classified, $mdSidenav) {
     $scope.showAdmin = true;
 
     Classified.getAll()
       .then(function(classifieds) {
         $scope.classifieds = classifieds;
       });
+
+    $scope.openSidenav = function() {
+      $mdSidenav('left').open();
+    };
+
+    $scope.closeSidenav = function() {
+      $mdSidenav('left').close();
+    };
   };
 
-  classifiedsCtrl.$inject = ['$scope', '$http', 'Classified'];
+  classifiedsCtrl.$inject = ['$scope', '$http', 'Classified', '$mdSidenav'];
 
   angular.module('ngClassifieds')
     .controller('ClassifiedsCtrl', classifiedsCtrl);
